2a8562b77302602cc9ec59a18e93445abab78ca0
James Armbrust <james.armbrust@gmail.com>
2025-08-04 11:03:05 -0600

    linkined link

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -2,7 +2,8 @@ export default function Resume() {
   return (
     <div className="flex-1 flex flex-col items-center justify-center w-full text-white">
       <h1 className="text-4xl font-bold mb-4">Resume</h1>
-      <p className="text-lg">TBD</p>
+      <p className="text-lg">Coming soon, but for now check out my LinkedIn profile for my latest resume and experience: &nbsp;
+        <a href="https://www.linkedin.com/in/jamesarmbrust/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">LinkedIn</a></p>
     </div>
   );
 }
