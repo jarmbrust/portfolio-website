6ed2237d3732b8467f45199cd8578843e5408318
James Armbrust <james.armbrust@gmail.com>
2025-08-04 11:21:31 -0600

    edited wording

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,20 +5,21 @@ export default function About() {
       <p className="text-lg mb-4">I am a software engineer, writer, gamer (table-top and online), animal-lover, and life-long learner.
         I originally started programming long ago with BASIC on the ancient computer my family could afford back when I was 12 years old. 
         However, I then took a break from programming when I went to college to study history. While I loved history (and still do!) I
-        decided to return to programming as I desired more of the logic, creativity, and problem solving that came with it.
-        Really, it was a more complex choice than that, but I will save those details for another time.
+        decided to return to programming as I enjoyed the logic, creativity, and problem solving that came with it.
       </p>
       <p className="text-lg mb-4">
         So, a few years after graduating with my history degree, I quit my job at the local library and went back to school.
         It was a huge challenge, as again I did this with no money and little support.  I lived in a tiny room in a rundown
         boarding house where I taught myself the basics of calculus in order to succeed in my math classes while working for
-        the university as many hours as I could manage. After almost three years of hard work, I graduated with a BS in Computer Science.
+        the university as many hours as I could manage. After about three years of hard work, I graduated with a BS in Computer Science.
       </p>
       <p className="text-lg mb-4">
-        Since that time, I have been working as a software engineer for a number of companies, including everything from start-ups to
-        larger corporations while utilizing a wide range of technologies, including SQL (SQL Server and PostgreSQL mostly), C#, Java,
-        jQuery, JavaScript, HTML, CSS, and more recently Vue.js, React, Next.js, Node.js, and TypeScript amongst others. At this point
-        in my career, I am mostly focused on front-end development, but I still enjoy working with back-end technologies as well.
+        Since that time, I have worked as a software engineer for a number of companies, including everything from start-ups to
+        larger corporations.  There I have utilized a wide range of technologies, including SQL (SQL Server and PostgreSQL mostly), C#, Java,
+        jQuery, JavaScript, HTML, CSS, and more recently Vue.js, React, Next.js, Node.js, and TypeScript amongst others. For a little over
+        the last five years I have been working mostly as a front-end developer. While I enjoy working on the front-end (and generally favor it),
+        I also am comfortable working on the back-end, and I am currently refreshing my SQL skills and digging into Node.js 
+        for possible full-stack opportunities.
       </p>
       <p className="text-lg mb-4">
         I live with my wife and our two dogs and three cats.  As of this writing, we are in the process of moving from Colorado 
