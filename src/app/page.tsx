ec8ca250d841887396a55f7f2fa5c77a60b6e31d
James Armbrust <james.armbrust@gmail.com>
2025-08-04 10:46:26 -0600

    a few text and layout changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
 
-  const imageOfMe = (meImage: string) => `/images/${meImage}`;
-
   return (
     <div className="flex flex-col items-center justify-center min-h-[70vh] gap-8">
       <Image
         className="rounded-full border-4 border-gray-800 shadow-lg mb-4"
-        src={imageOfMe("james_edit.jpeg")}
-        alt="James Image"
+        src="/images/james_edit.jpeg"
+        alt="James's Profile Image"
         width={120}
         height={120}
         priority
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,20 +1,25 @@
 export default function Projects() {
   return (
     <div className="flex-1 flex flex-col items-center justify-center w-full text-white">
-      <h1 className="text-4xl font-bold mb-4">Projects</h1>
-      <p className="text-lg mb-4">At this moment, there is one new project I am currently working on that I would like to showcase here.
+      <h1 className="text-4xl font-bold mb-6">Projects</h1>
+      <p className="w-full text-left text-2xl mb-2">
+        <a href="https://hire-an-adventurer.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Hire an Adventure</a>
+      </p>
+      <p className="text-lg mb-4">
+        At this moment, there is one new project I am currently working on that I would like to showcase here (more will be coming
+        as I fix up some old ones and add some new ones).
         It is in its second stage of development where the first iteration is &quot;done&quot;, but I am working to add new 
         features and improve the quality of the user experience.
-        It has an &quot;about&quot; page with more details about the project and upcoming features. While I used AI for some help 
-        with questions on syntax and so forth, 
-        for the most part I wrote it without much AI assistance.  I did this NOT to avoid AI, as I believe it is a great tool, 
-        but for personal projects where there is little in the way of time-crunch I wanted to practice and
-        keep my coding skills sharp by not relying on it too heavily.
       </p>
-      <p className="text-lg mb-4">While sort of based on my experience with e-commerce apps, it is a bit of a silly game,
-        but it was fun to build and I hope you enjoy it.  If you have any feedback, 
-        comments, or suggestions, please let me know.  You can find the game here (which has a link to the code on GitHub):&nbsp;
-        <a href="https://hire-an-adventurer.vercel.app/" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Hire an Adventure</a>.
+      <p className="text-lg mb-4">
+        While I used AI for some help with questions on syntax and so forth,
+        for the most part I wrote it with little AI assistance.  I did this NOT to avoid AI, as I believe it is a great tool,
+        but for personal projects where there is little in the way of time-crunch I wanted to
+        keep my coding skills sharp as possible by not relying on it too heavily.
+      </p>
+      <p className="text-lg mb-4">While this app is somewhat based on my experiences with e-commerce apps, ultimately it is a bit 
+        of a silly game.  However, I enjoyed making it was fun to build and I hope you enjoy it too.  If you have any feedback, 
+        comments, or suggestions, please let me know.
       </p>
     </div>
   );
