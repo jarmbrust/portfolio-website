f55e51f425c236e713c90ea8266320e962f82d35
James Armbrust <james.armbrust@gmail.com>
2025-08-04 11:46:31 -0600

    more rewording

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,11 +1,11 @@
 export default function About() {
   return (
     <div className="flex-1 flex flex-col items-center justify-center w-full text-white">
-      <h1 className="text-4xl font-bold mb-4">About Me</h1>
+      <h1 className="text-4xl font-bold mb-4">Who Am I?</h1>
       <p className="text-lg mb-4">I am a software engineer, writer, gamer (table-top and online), animal-lover, and life-long learner.
-        I originally started programming long ago with BASIC on the ancient computer my family could afford back when I was 12 years old. 
-        However, I then took a break from programming when I went to college to study history. While I loved history (and still do!) I
-        decided to return to programming as I enjoyed the logic, creativity, and problem solving that came with it.
+        I originally started programming long ago on the ancient computer my family could afford back when I was 12. 
+        Later however, I took a bit of a break from programming when I went to college to study history. While I loved history 
+        (and still do!) I decided to return to software creation as I enjoyed the logic, creativity, and problem solving that came with it.
       </p>
       <p className="text-lg mb-4">
         So, a few years after graduating with my history degree, I quit my job at the local library and went back to school.
@@ -15,14 +15,13 @@ export default function About() {
       </p>
       <p className="text-lg mb-4">
         Since that time, I have worked as a software engineer for a number of companies, including everything from start-ups to
-        larger corporations.  There I have utilized a wide range of technologies, including SQL (SQL Server and PostgreSQL mostly), C#, Java,
-        jQuery, JavaScript, HTML, CSS, and more recently Vue.js, React, Next.js, Node.js, and TypeScript amongst others. For a little over
-        the last five years I have been working mostly as a front-end developer. While I enjoy working on the front-end (and generally favor it),
-        I also am comfortable working on the back-end, and I am currently refreshing my SQL skills and digging into Node.js 
-        for possible full-stack opportunities.
+        somewhat large corporations.  There I have utilized a wide range of technologies, including SQL (SQL Server and PostgreSQL mostly),
+        C#, Java, jQuery, JavaScript, HTML, and CSS.  More recently (just over the last five years) I have focused my efforts in Vue.js,
+        React, Next.js, Node.js, and TypeScript, amongst others as a frontend developer.  While I am primarily focused on the frontend,
+        I am currently refreshing my SQL skills and digging a little more into Node.js for possible fullstack opportunities as well.
       </p>
       <p className="text-lg mb-4">
-        In my personal life, I live with my wife and our two dogs and three cats.  As of this writing (August 2025), 
+        In my personal life, I live with my awesome wife and our two dogs and three cats.  As of this writing (August 2025), 
         we are in the process of moving from Colorado to North Carolina to be closer to family.  
         I am currently looking for new opportunities in the area (or remotely) for this new chapter of our lives.
       </p>
