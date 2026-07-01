f95ec8b223e1009babdf1ba804f3df6a6954e78a
James Armbrust <jarmbrust@users.noreply.github.com>
2026-07-01 19:39:02 -0400

    small typo

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,7 +4,7 @@ export default function Projects() {
       <h1 className="flex justify-center items-center text-4xl font-bold mb-6">Projects</h1>
       <p className="w-full text-left text-2xl mb-2">
         Currently designing and developing a web app to allow users to store and share their favorite, and not so favorite recipes.
-        As of this writing, I&apos;ve completed the first full draft of the architecture document. You can find it &nbsp;
+        As of this writing, I&apos;ve completed the first full draft of the architecture document. You can find it&nbsp;
         <a href="https://github.com/jarmbrust/our-recipe-app/blob/main/docs/architecture_document.md" 
         target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-teal-500">here</a>.
       </p>
