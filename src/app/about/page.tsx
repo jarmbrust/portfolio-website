21c2599a69e77a5c0ea47f491a65d9bb4a0e6eac
James Armbrust <jarmbrust@users.noreply.github.com>
2026-07-01 19:26:40 -0400

    slight text rewrite and a few typos.

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,12 +2,14 @@ export default function About() {
   return (
     <div className="flex-1 flex flex-col items-center justify-center w-full">
       <h1 className="text-4xl font-bold mb-4">Who Am I?</h1>
-      <p className="text-lg mb-4">I am a software engineer, writer, gamer (table-top and online), animal-lover, and life-long learner.
-        Admittedly, I haven&apos;t published fiction in quite some time, but a few speculative fiction novellas, in addition to perhaps more &quote;pic&quot; works are 
-        rattling around in my noggin. Surprisingly, I am finding the trick about becoming a writer, is the whole &quote;writing&quot; thing. Gonna lock that down soon!
+      <p className="text-lg mb-4">A very important question to ask oneself, but not too early in the morning. Generally, in addition to being a software developer 
+        who loves creating and learning about code and technology, I also fancy myself a speculative fiction writer, an excellent TTRPG Game Master, and an all around decent human.
+        As for the writer part, admittedly I haven&apos;t actually published any fiction in quite some time. To be fair, I don&apos;t recall technically every publishing anything. But, 
+        there definitely are a few novellas, in addition to perhaps one or more &quote;epic&quot; works are rattling around in my noggin. Surprisingly, 
+        I am finding the trick about becoming a writer, is the whole &quote;writing&quot; thing. Gonna lock that down soon!
       </p>
       <p className="text-lg mb-4">
-        As for my career in software development, it goes back over ten years. While the last few have seen a focus mostly the frontend (in React and Vue, with TypeScript, etc),
+        As for my career in software development, it goes back well over ten years. While the last few have seen a focus mostly the frontend (in React and Vue, with TypeScript, etc),
         traditionally I&apos;ve worked throughout the entire stack. Now days, I&apos;m actually quite interested in returning to a more diverse toolset, even 
         if that is only in a subset of my role. I also have experience beyond IC roles, mentoring both mid and junior developers, as well as leading small development teams.
       </p>
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,8 +3,9 @@ export default function Projects() {
     <div className="flex-1 flex flex-col justify-center w-full">
       <h1 className="flex justify-center items-center text-4xl font-bold mb-6">Projects</h1>
       <p className="w-full text-left text-2xl mb-2">
-        Currently designing and developing a web app to allow users to store and share their favorite, and not so favorite, recipes.
-        As of this writing, I&apos;ve completed the first full draft of the architecture document. You can find it <a href="https://github.com/jarmbrust/our-recipe-app/blob/main/docs/architecture_document.md" 
+        Currently designing and developing a web app to allow users to store and share their favorite, and not so favorite recipes.
+        As of this writing, I&apos;ve completed the first full draft of the architecture document. You can find it 
+        <a href="https://github.com/jarmbrust/our-recipe-app/blob/main/docs/architecture_document.md" 
         target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-teal-500">here</a>.
       </p>
       <p>
