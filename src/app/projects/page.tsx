63efb00a275334947a2fa3142207cbae4de4ca8f
James Armbrust <jarmbrust@users.noreply.github.com>
2026-07-02 09:54:57 -0400

    grammar fixes

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -13,17 +13,18 @@ export default function Projects() {
           target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:text-teal-500">here</a>. 
         </p>
         <p className="mb-2">   
-          Beyond a demonstration of my development skills and a deep-dive into a few technologies I haven&apos;t taken the chance yet
-          to really dig into, the app&apos;s purpose is to allow users the ability to create an account to login in with in order to write, 
+          Beyond a demonstration of my development skills and a deep-dive into a few technologies I have a strong interest in, but haven&apos;t 
+          taken the opportunity to dig into yet, the app&apos;s purpose will be to allow users to create an account and log in with it to author, 
           store, modify, search, add notes to, and share their favorite, or if they choose not so favorite, recipes to others using the app.
-          You will also be able to categorize those recipes, and search for them based on those categories, or individual ingredients. 
+          The user will also be able to categorize those recipes, and search for them based on those categories. 
           Recipes will also have a feature to make them publicly accessible allowing them to be found without needing an account, 
           or private to the user only.
         </p>
         <p className="mb-2">
           Future plans for post-MVP include: searching the web for recipes that a freely available and importing them to your account, 
           the ability to weigh searches based on criteria not necessarily tagged, and maybe another category to allow sharing recipes with specific users.
-          Other modifications will occur as I think of them, or they are suggested to me.
+          Also, searching by individual ingredients is another priority I want to implement, and if time permits, may add to the MVP.
+          Other modifications will occur as I think of them, or when they are suggested by others.
         </p>
         <p className="mb-2">
           Currently, I do not have plans to monetize this app, mostly because there are apparently a ton of recipe apps out there.
